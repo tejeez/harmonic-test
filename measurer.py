cbf657660cca3caf38ce4202c5fb68ae701c1617
Tatu Peltola <tatu.peltola@gmail.com>
2021-02-28 17:29:06 +0200

    Test transmission and reception of bursts

--- measurer.py
+++ measurer.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 import SoapySDR
 from SoapySDR import *
+import numpy as np
 
 default_settings = {
     'device_args': {'driver': 'lime'},
+
     'rx_channel': 0,
     'tx_channel': 0,
     'rx_antenna': 'LNAH',
     'tx_antenna': 'BAND1',
     'rx_gains': (40,),
-    'tx_gains': (40,),
-    'samplerate': 1e6,
+    'tx_gains': (60,),
+
+    'samplerate': 1e5,
+
+    'rx_offset': 10000,  # RX frequency offset from TX
+
+    'burst_samples': 100,  # TX burst length in samples
+    'tx_amplitude': 1.0,
+    'tx_time': int(10e6), # How much into future TX burst is timed (nanoseconds), needed to deal with latency
+    'rx_samples': 2000,
 }
 
 class Measurer:
@@ -39,8 +49,44 @@ class Measurer:
         self.rxstream = self.sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
         self.txstream = self.sdr.setupStream(SOAPY_SDR_TX, SOAPY_SDR_CF32)
 
+        self.txburst = np.ones(settings['burst_samples'], dtype=np.complex64) * settings['tx_amplitude']
+
+    def measure(self, freq):
+        """Perform a measurement on a given frequency.
+
+        Transmitter LO is tuned to the frequency, and burst of the LO
+        frequency is transmitted.
+
+        Receiver LO is tuned to some offset from this frequency, which should
+        result in each harmonic being on a different "intermediate frequency".
+        """
+        self.sdr.setFrequency(SOAPY_SDR_RX, self.settings['rx_channel'], freq + self.settings['rx_offset'])
+        self.sdr.setFrequency(SOAPY_SDR_TX, self.settings['tx_channel'], freq)
+
+        self.sdr.activateStream(self.txstream)
+        self.sdr.activateStream(self.rxstream)
+        tnow = self.sdr.getHardwareTime()
+
+        rt = self.sdr.writeStream(
+            self.txstream,
+            (self.txburst,), len(self.txburst),
+            flags = SOAPY_SDR_END_BURST | SOAPY_SDR_HAS_TIME,
+            timeNs = tnow + self.settings['tx_time'])
+        print("TX:", rt)
+
+        rxbuf = np.zeros(self.settings['rx_samples'], dtype=np.complex64)
+        rr = self.sdr.readStream(self.rxstream, (rxbuf,), len(rxbuf))
+        print("RX:", rr)
+        print(np.mean(np.abs(rxbuf).reshape(-1, 100), axis=1))
+
+        self.sdr.deactivateStream(self.txstream)
+        self.sdr.deactivateStream(self.rxstream)
+
+
 def test():
     measurer = Measurer(default_settings)
+    for f in np.linspace(2.4e9, 2.45e9, 50):
+        measurer.measure(f)
 
 if __name__ == '__main__':
     test()
